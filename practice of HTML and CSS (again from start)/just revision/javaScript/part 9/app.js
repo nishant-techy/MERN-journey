e901fea036859dd027eec50e05047eb42463d042
nishant-source <nishantsaini101243@gmail.com>
2026-05-10 12:12:57 +0530

    JS part-9 completed

--- practice of HTML and CSS (again from start)/just revision/javaScript/part 9/app.js
+++ practice of HTML and CSS (again from start)/just revision/javaScript/part 9/app.js
@@ -22,3 +22,34 @@ pp.innerHTML = "ME TOO!";
 h3.insertAdjacentElement("afterend", div); // jo bhi element bnaya h usko append krna sabse important h
 div.appendChild(heading);
 div.appendChild(pp);
+
+// Assignment Questions
+// 1st question
+let input = document.createElement("input");
+let btn = document.createElement("button");
+btn.innerText = "Click me!";
+// let's make something global which really helps us, everytime we want to use it
+let body = document.querySelector("body");
+document.querySelector("body").append(input);
+document.querySelector("body").append(btn);
+
+// 2nd question
+input.id = "InputText";
+input.placeholder = "username";
+btn.id = "btn";
+
+// 3rd question
+document.querySelector("#btn").style.backgroundColor = "blue";
+document.querySelector("#btn").style.color = "white";
+
+// 4th question
+let headingg = document.createElement("h1");
+headingg.innerText = "DOM Practice";
+headingg.style.color = "purple";
+headingg.style.textDecoration = "underlined";
+body.insertAdjacentElement("beforeend", headingg);
+
+// 5th question
+let pTag = document.createElement("p");
+pTag.innerHTML = "Apna College <b>Delta</b> Practice";
+body.appendChild(pTag);
