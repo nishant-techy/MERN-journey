e194d36d8497ff1fd43200c4bf397422b73cb3f8
Nishant <nishantsainih@gmail.com>
2026-06-04 12:49:23 +0530

    Auto update Thu 06/04/2026 12:49:23.25

--- javaScript/part 12/app.js
+++ javaScript/part 12/app.js
@@ -2,11 +2,14 @@
 
 let url = "https://v2.jokeapi.dev/joke/Any?safe-mode";
 
+let listParent = document.querySelector("ul");
 async function access() {
   let res = await fetch(url);
   let data = await res.json();
-  console.log(data.setup);
-  console.log(data.delivery);
+
+  let list = document.createElement("li");
+  list.innerHTML = `<b> ${data.setup} - ${data.delivery}`;
+  listParent.appendChild(list);
 }
 
 access();
