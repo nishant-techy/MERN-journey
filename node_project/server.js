c80f36fc9d612f2b7e522a4e45305edad9b26462
Nishant <nishantsainih@gmail.com>
2026-06-20 10:59:33 +0530

    Auto update Sat 06/20/2026 10:59:33.37

--- node_project/server.js
+++ node_project/server.js
@@ -4,5 +4,13 @@ const app = express();
 let port = 8080;
 
 app.listen(port, () => {
-  alert("We're listening to your request at the port: ", port);
+  console.log("We're listening to your request at the port: ", port);
+});
+
+//we should listen the request coming from the user side, so, use the listen
+// app.use is the middleware, not for starting the server.
+// and app.get use for telling the exact destination, the exact route
+
+app.get("/", (req, res) => {
+  res.send("Request recieved"); // response send this and this
 });
